21ae76f6ebd8b8e5df8721268a19802d32b46c00
Hycarus <mirkomarasco08@gmail.com>
2023-12-26 09:32:54 +0900

    added data in train seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -16,5 +16,21 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        for ($i = 0; $i < 10; $i++) {
+
+            $train = new Train();
+            $train->compagnia = $faker->company();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->data_partenza = $faker->dateTimeBetween('-1 day', '+1 day');
+            $train->ora_partenza = $faker->time();
+            $train->data_arrivo = $faker->dateTimeBetween('-1 day', '+1 day');
+            $train->ora_arrivo = $faker->time();
+            $train->codice_treno = $faker->ean13();
+            $train->numero_carrozze = $faker->numberBetween(1, 20);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
